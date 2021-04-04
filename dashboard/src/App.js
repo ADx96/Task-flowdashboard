a044fa35a921044aec3f40c490a6a20fd0d10182
ADx96 <aboodrous@gmail.com>
2021-04-05 00:07:01 +0300

    dash board starter

--- dashboard/src/App.js
+++ dashboard/src/App.js
@@ -1,7 +1,24 @@
-import SignInSide from "./Components/Signin";
+import ResponsiveDrawer from "./Components/Drawers";
+import React from "react";
+import "./App.css";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import AddEmployee from "./Pages/AddEmployee";
+import Employees from "./Pages/Employees";
+import MangaeTasks from "./Pages/ManageTasks";
 
 function App() {
-  return <SignInSide />;
+  return (
+    <div className="App">
+      <Router>
+        <ResponsiveDrawer />
+        <Switch>
+          <Route path="/" exact component={AddEmployee} />
+          <Route path="/Employees" exact component={Employees} />
+          <Route path="/MangaeTasks" exact component={MangaeTasks} />
+        </Switch>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
